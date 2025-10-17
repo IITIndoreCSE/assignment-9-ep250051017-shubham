1369ec60d6eed8a55c15ee562f3b0b816136187a
ep250051017-shubham <ep250051017@iiti.ac.in>
2025-10-17 16:09:51 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+struct Point
+{
+    float x, y;
+};
+
+int main() {
+   
+    Point arr[7];
+    cout<<"Enter coordinates for 7 points (x, y): \n";
+    for(int i = 0; i < 7; i++)
+    {
+        float x, y;
+        cout << "Point " << i+1 <<"-x:";
+        cin >> x;
+        cout << "Point " << i+1 <<"-x:";
+        cin >> y;
+        arr[i] = {x, y};
+       
+    }
+   
+    int num=0;
+   
+    for ( int i = 0; i < 7; i++)
+    {
+        if (arr[i].x > 0 && arr[i].y > 0)
+        {
+            num++;
+        }
+    }
+    cout << "Numbers of points in first quadrant:"<< num <<endl;
+    
+}
