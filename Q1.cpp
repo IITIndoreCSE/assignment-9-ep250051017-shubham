d13a52724d9c7a69c274099bf2e39de13d90f448
ep250051017-shubham <ep250051017@iiti.ac.in>
2025-10-17 15:55:25 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+struct Item{
+    int id;
+    float cost;
+};
+
+int main() {
+   
+    Item arr[6];
+    for(int i = 0; i<4; i++)
+    {
+        arr[i].id = i+1;
+        arr[i].cost = (i+1)*20;
+    }
+    cout << "Details of items with cost more than 50 are : \n";
+    for(int i = 0; i < 6; i++)
+    {
+        if(arr[i].cost > 50)
+        {
+            cout << "ID : "<<arr[i].id <<", cost : "<<arr[i].cost << endl;
+        }
+    }
+
+}
